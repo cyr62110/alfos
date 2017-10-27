9422893484e6f773fa04f8aece30464b13571e06
Cyril Vlaminck <cyril@adbuddiz.com>
2017-10-27 15:15:06 +0200

    First thought about alfos API

---
+++ alfos-core/src/main/kotlin/fr/cvlaminck/alfos/model/StorageCollection.kt
@@ -0,0 +1,12 @@
+package fr.cvlaminck.alfos.model
+
+/**
+ * Provide all information on a collection of objects stored in a storage.
+ */
+interface StorageCollection {
+
+    /**
+     * Name of the collection.
+     */
+    val name: String
+}

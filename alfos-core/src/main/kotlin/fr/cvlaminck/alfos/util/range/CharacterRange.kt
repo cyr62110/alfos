637eca1f2c82eb376d95b9a54d2115ae517000dc
Cyril Vlaminck <vlaminck.cyril@gmail.com>
2017-11-08 07:59:10 +0100

    Added unicode support to character range.

--- alfos-core/src/main/kotlin/fr/cvlaminck/alfos/util/range/CharacterRange.kt
+++ alfos-core/src/main/kotlin/fr/cvlaminck/alfos/util/range/CharacterRange.kt
@@ -3,27 +3,41 @@ package fr.cvlaminck.alfos.util.range
 /**
  * A range of characters.
  *
- * @constructor Create a new range containing all characters between [startCharacter] and [endCharacter] inclusive.
- * @param startCharacter First character contained in the range.
- * @param endCharacter Last character contained in the range. Unicode code of this character must be greater than [startCharacter].
+ * @constructor Create a new range containing all characters between [startCharacterCodePoint] and [endCharacterCodePoint] inclusive.
+ * @param startCharacterCodePoint Unicode code point of the first character contained in the range.
+ * @param endCharacterCodePoint Unicode code point of the last character contained in the range. Must be greater than [startCharacterCodePoint].
  */
 class CharacterRange(
-        val startCharacter: Char,
-        val endCharacter: Char
+        val startCharacterCodePoint: Int,
+        val endCharacterCodePoint: Int
 ) {
+
+    /**
+     * Create a new range containing all characters between [startCharacter] and [endCharacter] inclusive.
+     * @param startCharacter First character contained in the range.
+     * @param lastCharacter Last character contained in the range.
+     */
+    constructor(startCharacter: Char, endCharacter: Char) : this(startCharacter.toInt(), endCharacter.toInt())
 
     /**
      * Create a new range including [char] as only character.
      *
      * @param char Only character contained in the range.
      */
-    constructor(char: Char) : this (char, char)
+    constructor(char: Char) : this(char, char)
 
     /**
-     * Returns true if the unicode code of the provided [char] is a number between the unicode codes of the [startCharacter] and
-     * the [endCharacter].
+     * Returns true if the unicode code of the provided [char] is between [startCharacterCodePoint] and
+     * [endCharacterCodePoint].
      *
      * @return true if the [char] is in the range, false otherwise.
      */
-    fun contains(char: Char): Boolean = char in startCharacter..endCharacter
+    fun contains(char: Char): Boolean = char.toInt() in startCharacterCodePoint..endCharacterCodePoint
+
+    /**
+     * Return true if the provided [codePoint] is between [startCharacterCodePoint] and [endCharacterCodePoint].
+     *
+     * @return true if the [codePoint] is in the range, false otherwise.
+     */
+    fun contains(codePoint: Int): Boolean = codePoint in startCharacterCodePoint..endCharacterCodePoint
 }
--- alfos-core/src/main/kotlin/fr/cvlaminck/alfos/util/range/CharacterRanges.kt
+++ alfos-core/src/main/kotlin/fr/cvlaminck/alfos/util/range/CharacterRanges.kt
@@ -20,6 +20,14 @@ class CharacterRanges private constructor (
     fun contains(character: Char): Boolean = characterRanges.any { it.contains(character) }
 
     /**
+     * Returns true if the Unicode [characterCodePoint] is contained in one of the [CharacterRange] contained in this collection.
+     *
+     * @param characterCodePoint Unicode code point of the character to check.
+     * @return true if [character] is contained in one [CharacterRange], false otherwise.
+     */
+    fun contains(characterCodePoint: Int): Boolean = characterRanges.any { it.contains(characterCodePoint) }
+
+    /**
      * Create a new [Builder] already containing all the [CharacterRange] contained in this collection.
      *
      * @return a new [Builder].

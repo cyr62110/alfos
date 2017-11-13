8cda157ff97a47831152e5334bc87c073a5c7d2a
Cyril Vlaminck <vlaminck.cyril@gmail.com>
2017-11-13 08:13:18 +0100

    Added lots of function on the path builder

--- alfos-core/src/main/kotlin/fr/cvlaminck/alfos/core/name/path/StorageObjectPath.kt
+++ alfos-core/src/main/kotlin/fr/cvlaminck/alfos/core/name/path/StorageObjectPath.kt
@@ -1,14 +1,26 @@
 package fr.cvlaminck.alfos.core.name.path
 
+import fr.cvlaminck.alfos.core.name.path.builder.StorageObjectPathBuilder
+import fr.cvlaminck.alfos.name.NameEncoder
 import fr.cvlaminck.alfos.name.NameValidator
 
 /**
  * Representation of the name of the object as a virtual hierarchy in a collection.
  *
- * All the object having the same leading path segments will be considered as grouped.
+ * All the path are absolute but they not start with a path separator.
+ * The root directory contains all object having a name without segment separator. All the object having the same
+ * leading path segments will be considered as grouped.
+ *
+ * Examples:
+ * - test.txt -> 'test.txt' in the root directory
+ * - A/test.txt -> 'test.txt' in 'A' directory at the root.
+ * - A/B/test.txt -> 'test.txt' in 'A' directory in 'B' directory at the root.
  */
-class StorageObjectPath(
+class StorageObjectPath internal constructor(
         val nameValidator: NameValidator,
+        val nameEncoder: NameEncoder,
         val segments: List<String>
 ) {
+
+    fun buildUpon(): StorageObjectPathBuilder = StorageObjectPathBuilder(nameValidator, nameEncoder, segments)
 }
--- alfos-core/src/main/kotlin/fr/cvlaminck/alfos/core/name/path/builder/StorageObjectPathBuilder.kt
+++ alfos-core/src/main/kotlin/fr/cvlaminck/alfos/core/name/path/builder/StorageObjectPathBuilder.kt
@@ -5,30 +5,87 @@ import fr.cvlaminck.alfos.name.NameEncoder
 import fr.cvlaminck.alfos.name.NameValidator
 
 /**
+ * Builder class that provides method allowing to easily manipulate object name as virtual path.
  *
  * @param nameValidator
  * @param nameEncoder
  */
-class StorageObjectPathBuilder (
+class StorageObjectPathBuilder internal constructor(
         private val nameValidator: NameValidator,
         private val nameEncoder: NameEncoder
 ) {
-    private val segments: MutableList<String> = arrayListOf()
+    private val segments: MutableList<String> = mutableListOf()
 
-    fun appendPathSegment(segment: String): StorageObjectPathBuilder {
-        val encodedSegment = nameEncoder.encodePathSegmentInObjectName(segment)
-        return appendEncodedPathSegment(encodedSegment)
+    internal constructor(nameValidator: NameValidator, nameEncoder: NameEncoder, segments: Collection<String>) : this(nameValidator, nameEncoder) {
+        this.segments.addAll(segments)
     }
 
-    fun appendEncodedPathSegment(segment: String): StorageObjectPathBuilder {
+    /**
+     * Returns the number of segments in the path that will be build.
+     */
+    val segmentCount: Int
+        get() = segments.size
+
+    /**
+     * Append a new segment to the path.
+     *
+     * Segment will be encoded using [nameEncoder] and validated using [nameValidator].
+     *
+     * @param segment Segment to add to the path.
+     * @return this [StorageObjectPathBuilder].
+     */
+    fun appendPathSegment(segment: String): StorageObjectPathBuilder {
+        val encodedSegment = nameEncoder.encodePathSegmentInObjectName(segment)
+        return appendRawPathSegment(encodedSegment)
+    }
+
+    /**
+     * Append a path segment to the path.
+     *
+     * The segment will not be encoded but still will be validated using [nameValidator].
+     *
+     * @param segment Segment to add to the path.
+     * @return this [StorageObjectPathBuilder].
+     */
+    fun appendRawPathSegment(segment: String): StorageObjectPathBuilder {
         nameValidator.validatePathSegmentInObjectName(segment)
         segments.add(segment)
         return this
     }
 
-    fun build(): StorageObjectPath {
-        TODO("Implements")
+    /**
+     * Remove the path segment at [index].
+     *
+     * @param index Index of the segment to remove.
+     * @throws IllegalArgumentException thrown if the [index] is below 0 or equals or greater than the [segmentCount].
+     * @return this [StorageObjectPathBuilder].
+     */
+    fun removePathSegmentAt(index: Int): StorageObjectPathBuilder {
+        if (index < 0 || index >= segmentCount) {
+            throw IllegalArgumentException("Index ${index} is outside the range [0, ${segmentCount}[.")
+        }
+        segments.removeAt(index)
+        return this
     }
+
+    /**
+     * Remove the last path segment. Do nothing is the path is empty.
+     *
+     * @return this [StorageObjectPathBuilder].
+     */
+    fun removeLastPathSegment(): StorageObjectPathBuilder {
+        if (segmentCount > 0) {
+            segments.removeAt(segmentCount - 1)
+        }
+        return this
+    }
+
+    /**
+     * Build a new [StorageObjectPath].
+     *
+     * @return a new [StorageObjectPath].
+     */
+    fun build(): StorageObjectPath = StorageObjectPath(nameValidator, nameEncoder, segments)
 
     companion object {
         val PATH_SEPARATOR = "/"

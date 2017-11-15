6d00bce8579bd55b7e428c84ab8588e1507db40f
Cyril Vlaminck <cyril@adbuddiz.com>
2017-11-15 18:39:40 +0100

    Improved documentation for new methods.

--- alfos-core/src/main/kotlin/fr/cvlaminck/alfos/core/StorageObjectNameManager.kt
+++ alfos-core/src/main/kotlin/fr/cvlaminck/alfos/core/StorageObjectNameManager.kt
@@ -5,18 +5,20 @@ import fr.cvlaminck.alfos.core.name.path.builder.StorageObjectPathBuilder
 import fr.cvlaminck.alfos.core.name.validator.SafeNameValidator
 
 /**
- *
+ * 
  */
 class StorageObjectNameManager(
         private val storageRegistry: StorageRegistry
 ) {
 
     /**
-     * Create a new [StorageObjectPathBuilder] that will validate and encode segments according to the naming
-     * rules of the given provider.
+     * Create a new [StorageObjectPathBuilder] that will validate segments according to the naming rules of
+     * the given provider.
      *
-     * [StorageObjectPath] created using returned [StorageObjectPathBuilder] may not be usable for any other provider.
-     * If you want your object to have an interoperable naming, use [newSafePathBuilder] instead.
+     * The [StorageObjectPathBuilder] will not transform the object name, it will only validate it against the naming
+     * rule of the provider. As it is validated against the provider naming rules, the generated object name may only
+     * be usable for this provider. If your goal is to generate an object name that is usable accross providers, use
+     * [newSafePathBuilder] instead.
      *
      * @param providerName Name of the provider on which the object is/will be hosted.
      * @return a new [StorageObjectPathBuilder] configured for the given provider.
@@ -26,8 +28,10 @@ class StorageObjectNameManager(
     }
 
     /**
-     * Create a [StorageObjectPathBuilder] that will validate and encode segments according to naming rules that
-     * allow the object to have the exact same name on all providers supported by the library.
+     * Create a [StorageObjectPathBuilder] that will validate and encode segments according to safe naming rules.
+     *
+     * A safe [StorageObjectPathBuilder] will encode segments using a [SafeNameEncoder]. This allow the object
+     * to have the same name across the different provider.
      *
      * @return a new [StorageObjectPathBuilder] configure with a [SafeNameEncoder] and a [SafeNameValidator].
      */

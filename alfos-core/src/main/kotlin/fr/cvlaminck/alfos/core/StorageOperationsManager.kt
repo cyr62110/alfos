6068aa64a7b05804c7cc03255e9282e37d7cfab5
Cyril Vlaminck <cyril@adbuddiz.com>
2017-12-16 10:53:22 +0100

    Introduced new core element: object uri factory that allow to create validated uri.

--- alfos-core/src/main/kotlin/fr/cvlaminck/alfos/core/StorageOperationsManager.kt
+++ alfos-core/src/main/kotlin/fr/cvlaminck/alfos/core/StorageOperationsManager.kt
@@ -1,14 +1,16 @@
 package fr.cvlaminck.alfos.core
 
-import fr.cvlaminck.alfos.model.Storage
 import fr.cvlaminck.alfos.core.name.path.StorageObjectUri
+import fr.cvlaminck.alfos.model.Storage
 import fr.cvlaminck.alfos.operation.StorageCollectionOperations
 import fr.cvlaminck.alfos.operation.StorageObjectOperations
 import fr.cvlaminck.alfos.operation.StorageOperations
 import io.reactivex.Single
 
 class StorageOperationsManager(
-        val registry: StorageRegistry
+        private val registry: StorageRegistry,
+        private val objectNameFactory: StorageObjectNameFactory,
+        private val uriFactory: StorageObjectUriFactory
 ) {
     fun getStorageOperations(storage: Storage): Single<StorageOperations> {
         TODO("Implements")
@@ -18,13 +20,15 @@ class StorageOperationsManager(
         TODO("Implements")
     }
 
-    fun getCollectionOperations(uri: String): Single<StorageCollectionOperations> = StorageObjectUri.fromString(uri)
-            .flatMap { uri -> getCollectionOperations(uri) }
+    fun getCollectionOperations(uri: String): Single<StorageCollectionOperations> {
+        TODO("Implements")
+    }
 
     fun getObjectOperations(uri: StorageObjectUri): Single<StorageObjectOperations> {
         TODO("Implements")
     }
 
-    fun getObjectOperations(uri: String): Single<StorageObjectOperations> = StorageObjectUri.fromString(uri)
-            .flatMap { uri -> getObjectOperations(uri) }
+    fun getObjectOperations(uri: String): Single<StorageObjectOperations> {
+        TODO("Implements")
+    }
 }
